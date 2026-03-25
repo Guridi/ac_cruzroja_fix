1a4444a2b4f7232219d5ab102ca02127eab75b6d
Antonio Guridi <39349521+Guridi@users.noreply.github.com>
2026-03-25 10:23:00 -0400

    Change version from 18.0.1.0.0 to 17.0.1.0.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Cruz Roja Dominicana - Personalización de Reportes',
-    'version': '18.0.1.0.0',
+    'version': '17.0.1.0.0',
     'summary': 'Personalización de reportes de facturas, compras y ventas para Cruz Roja',
     'description': """
         Personalización de Reportes Cruz Roja Dominicana
